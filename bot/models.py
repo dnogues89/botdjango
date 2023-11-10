cdd84692582299a0156680f238cc4a10a2da2eda
dnogues89 <damiannogues@gmail.com>
2023-11-10 15:33:08 -0300

    asd

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MensajesRecibidos(models.Model):
+    id_wa = models.CharField(max_length=100, unique=True)
+    mensaje = models.TextField()
+    timestamp = models.IntegerField()
+    telefono_cliente = models.CharField(max_length=100)
+    telefono_receptor = models.CharField(max_length=100)
+    
--- bot/views.py
+++ bot/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
+from .models import MensajesRecibidos
+
+import json
 
 
 # Create your views here.
@@ -15,4 +18,22 @@ def webhook(request):
             # SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
             return HttpResponse("Error de autentificacion.")
     
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+        if data['entry'][0]['changes'][0]['value']['messages'][0]['type']=='text':
+            #EXTRAEMOS EL NUMERO DE TELEFONO Y EL MANSAJE
+            telefonoCliente=data['entry'][0]['changes'][0]['value']['messages'][0]['from']
+            #EXTRAEMOS EL TELEFONO DEL CLIENTE
+            mensaje=data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+            #EXTRAEMOS EL ID DE WHATSAPP DEL ARRAY
+            idWA=data['entry'][0]['changes'][0]['value']['messages'][0]['id']
+            #EXTRAEMOS EL TIEMPO DE WHATSAPP DEL ARRAY
+            timestamp=data['entry'][0]['changes'][0]['value']['messages'][0]['timestamp']
+            MensajesRecibidos.objects.create(id_wa=idWA,mensaje=mensaje,timestamp=timestamp,telefono_cliente=telefonoCliente,telefono_receptor='baires')
+            
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Error al decodificar JSON"}, status=400)
+
+    
+        
     return HttpResponse("ACA ESOTY")
