6428cf06ba247e0e3490dc8a8d1b6d61c728fd07
dnogues89 <damiannogues@gmail.com>
2023-11-10 15:16:46 -0300

    ads

--- bot/views.py
+++ bot/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+
 
 # Create your views here.
 
 def webhook(request):
-    #SI HAY DATOS RECIBIDOS VIA GET
+    # SI HAY DATOS RECIBIDOS VIA GET
     if request.method == "GET":
-        #SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
-        if request.args.get('hub.verify_token') == "FransiBOT":
-            #ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
-            return request.args.get('hub.challenge')
+        # SI EL TOKEN ES IGUAL AL QUE RECIBIMOS
+        if request.GET.get('hub.verify_token') == "FransiBOT":
+            # ESCRIBIMOS EN EL NAVEGADOR EL VALOR DEL RETO RECIBIDO DESDE FACEBOOK
+            return HttpResponse(request.GET.get('hub.challenge'))
         else:
-            #SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
-            return "Error de autentificacion."
+            # SI NO SON IGUALES RETORNAMOS UN MENSAJE DE ERROR
+            return HttpResponse("Error de autentificacion.")
     
-    return JsonResponse({"asd":"asd"})
+    return HttpResponse("ACA ESOTY")
