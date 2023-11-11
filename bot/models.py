196a9bd7dcb3cd7d92535eba0dc270a1b427d9bf
dnogues89 <damiannogues@gmail.com>
2023-11-11 09:41:41 -0300

    asd

--- bot/models.py
+++ bot/models.py
@@ -7,6 +7,34 @@ class MensajesRecibidos(models.Model):
     timestamp = models.IntegerField()
     telefono_cliente = models.CharField(max_length=100)
     telefono_receptor = models.CharField(max_length=100)
+    creado = models.DateTimeField(auto_created=True)
+
+    def __str__(self) -> str:
+        return self.telefono_cliente
+    
+    class Meta:
+        verbose_name = 'mensaje'
+        verbose_name_plural = 'mensajes'
     
 class Error(models.Model):
     error = models.TextField()
+    
+class Cliente(models.Model):
+    telefono = models.IntegerField()
+    nombre = models.CharField(max_length=50, blank=True,null=True)
+    email = models.CharField(max_length=50, blank=True,null=True)
+    flow = models.IntegerField(blank=True,null=True)
+    modelo = models.CharField(max_length=50, blank=True,null=True)
+    canal = models.CharField(max_length=50, blank=True,null=True)
+    contacto = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return f''
+    
+    class Meta:
+        verbose_name = 'mensaje'
+        verbose_name_plural = 'mensajes'
+    
+class Flow(models.Model):
+    flow_id = models.IntegerField()
+    respuesta = models.TextField()
