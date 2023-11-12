62d8c684ac6cb11d49809a2e5f179e50af51e533
dnogues89 <damiannogues@gmail.com>
2023-11-12 09:20:35 -0300

    asd

--- bot/admin.py
+++ bot/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from .models import MensajesRecibidos, Error, Flow, Cliente,Key
 
 # Register your models here.
-admin.site.register(MensajesRecibidos)
+class MensajesRecibidosAdmin(admin.ModelAdmin):
+    list_display=('telefono_cliente','mensaje','creado','id_wa')
+
+admin.site.register(MensajesRecibidos, MensajesRecibidosAdmin)
 admin.site.register(Error)
 admin.site.register(Flow)
 admin.site.register(Cliente)
