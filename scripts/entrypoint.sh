c46fd7662ee7e6e903220968f3271cc22d655aff
Kevin E. Minino <kevin.minino@muttdata.ai>
2023-12-05 11:28:58 -0300

    Testing how to add make migrations and changing entrypoint.sh

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./manage.py collectstatic --noinput
+# i commit my migration files to git so i dont need to run it on server
+# ./manage.py makemigrations app_name
+./manage.py migrate
+
+# here it start nginx and the uwsgi
+#supervisord -c /etc/supervisor/supervisord.conf -n
+
+# Run Server
+python manage.py runserver 0.0.0.0:8000
