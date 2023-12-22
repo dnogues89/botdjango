4131ddecacdd57afe1386b730ef3ad527fad37b2
dnogues89 <damiannogues@gmail.com>
2023-12-22 11:33:52 -0300

    asd

--- bot/views.py
+++ bot/views.py
@@ -223,7 +223,7 @@ def clientes_abandonados(request):
     # Obtén la fecha y hora actual en la zona horaria de Buenos Aires
     clientes = Cliente.objects.filter(flow=50)
     
-    clientes_filtrados = clientes.filter(contacto__lte=datetime.now()+timedelta(hours=3,minutes=10))
+    clientes_filtrados = clientes.filter(contacto__lte=datetime.now()+timedelta(hours=3,minutes=30))
     
     for cliente in clientes_filtrados:
         cliente.flow = 30
