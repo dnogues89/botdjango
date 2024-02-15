9b8771fd9fea9ad617df7dffcf25bc50f87127b5
dnogues89 <damiannogues@gmail.com>
2024-02-15 14:18:48 -0300

    asd

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 # i commit my migration files to git so i dont need to run it on server
 # ./manage.py makemigrations app_name
 python manage.py makemigrations
