8cdfa268cfabcafe6f97487a09ea6e3a70fe1a87
dnogues89 <damiannogues@gmail.com>
2024-03-27 13:46:11 -0300

    asd

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,8 +3,8 @@
 python manage.py collectstatic --no-input
 # i commit my migration files to git so i dont need to run it on server
 # ./manage.py makemigrations app_name
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # here it start nginx and the uwsgi
 #supervisord -c /etc/supervisor/supervisord.conf -n
